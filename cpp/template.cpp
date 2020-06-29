2df3402191b8875aa9ad5efa878172b19af9f2f6
root <walker2002226@gmail.com>
2020-06-29 15:59:38 +0000

    fix header

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,16 +1,5 @@
 // header file section
-#include <iostream>
-#include <cstdio>
-#include <cfloat>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <bitset>
-#include <functional>
-#include <numeric>
-#include <algorithm>
+#include "bits/stdc++.h"
 
 using namespace std;
 using llong = long long;
