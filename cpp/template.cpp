49861c6db4a25b39c6a3867f6d5cfb4b4b051a08
root <walker2002226@gmail.com>
2020-08-05 11:26:39 +0000

    add repeat

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,11 +1,13 @@
-// header file section
 #include "bits/stdc++.h"
+
+#define _overload(_1, _2, _3, _4, name, ...) name
+#define _rep1(Itr, N) _rep3(Itr, 0, N, 1)
+#define _rep2(Itr, a, b) _rep3(Itr, a, b, 1)
+#define _rep3(Itr, a, b, step) for (i64 (Itr) = a; (Itr) < b; (Itr) += step)
+#define repeat(...) _overload(__VA_ARGS__, _rep3, _rep2, _rep1)(__VA_ARGS)
 
 using namespace std;
 using llong = long long;
-
-// library section
-// ===
 
 int main() {
     return 0;
